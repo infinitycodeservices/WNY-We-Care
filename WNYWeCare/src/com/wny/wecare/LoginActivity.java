dfdebdb8077770f05f2b30e446378bdef571c5af
Scott DeCota <infinitycodeservices@gmail.com>
2014-11-07 08:39:48 -0500

    Update LoginActivity.java
    Update

--- WNYWeCare/src/com/wny/wecare/LoginActivity.java
+++ WNYWeCare/src/com/wny/wecare/LoginActivity.java
@@ -1,60 +1,34 @@
 package com.wny.wecare;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.IntentSender.SendIntentException;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.wny.wecare.R;
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.UiLifecycleHelper;
-import com.facebook.model.GraphUser;
-import com.facebook.widget.LoginButton;
-import com.facebook.widget.LoginButton.UserInfoChangedCallback;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.plus.Plus;
-import com.google.android.gms.plus.model.people.Person;
 import com.wny.wecare.handler.JSONParser;
 
 
-public class LoginActivity extends Activity implements OnClickListener,
-ConnectionCallbacks, OnConnectionFailedListener {
+public class LoginActivity extends Activity implements OnClickListener {
 
 	// Progress Dialog
 	private ProgressDialog pDialog;
 
 	JSONParser jsonParser = new JSONParser();
-	EditText inputName;
 	EditText inputEmail;
 	Button btnLogin;
 
@@ -62,407 +36,121 @@ ConnectionCallbacks, OnConnectionFailedListener {
 	private static String url_create_user = "http://infinitycodeservices.com/create_user.php";
 
 	// JSON Node names
-	private static final String TAG_SUCCESS = "success";
-	private static final int RC_SIGN_IN = 0;
+		private static final String TAG_SUCCESS = "success";
 
-	// Profile pic image size in pixels
-	private static final int PROFILE_PIC_SIZE = 400;
-
-	// Google client to interact with Google API
-	private GoogleApiClient mGoogleApiClient;
-
-	/**
-	 * A flag indicating that a PendingIntent is in progress and prevents us
-	 * from starting further intents.
-	 */
-	private boolean mIntentInProgress;
-
-	private boolean mSignInClicked;
-
-	private ConnectionResult mConnectionResult;
-
-	private SignInButton btnSignIn;
-
-	private ImageView imgProfilePic;
-	private TextView txtName, txtEmail;
-	private LinearLayout llProfileLayout;
-
-	public static final String TAG = LoginActivity.class.getSimpleName();
-
-
-	private UiLifecycleHelper uiHelper;
-
-	private Session.StatusCallback sessionStatusCallback = new Session.StatusCallback() {
 		@Override
-		public void call(Session session, SessionState state,
-				Exception exception) {
-			onSessionStateChange(session, state, exception);
-		}
-	};
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.login_activity);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login_activity);
-		uiHelper = new UiLifecycleHelper(this, sessionStatusCallback);
-		uiHelper.onCreate(savedInstanceState);
-
-		// Create Button
-		btnLogin = (Button) findViewById(R.id.btnEmail);
-		btnSignIn = (SignInButton) findViewById(R.id.gplus);
-
-		// Edit Text
-		txtName = (TextView) findViewById(R.id.txtName);
-		txtEmail = (TextView) findViewById(R.id.txtEmail);
-		llProfileLayout = (LinearLayout) findViewById(R.id.llProfile);
-
-
-		// Button click listeners
-		btnSignIn.setOnClickListener(this);
-		btnLogin.setOnClickListener(this);	
-
-		mGoogleApiClient = new GoogleApiClient.Builder(this)
-		.addConnectionCallbacks(this)
-		.addOnConnectionFailedListener(this).addApi(Plus.API, null)
-		.addScope(Plus.SCOPE_PLUS_LOGIN).build();
-	}
-
-	protected void onStart() {
-		super.onStart();
-		mGoogleApiClient.connect();
-	}
-
-	protected void onStop() {
-		super.onStop();
-		if (mGoogleApiClient.isConnected()) {
-			mGoogleApiClient.disconnect();
-		}
-	}
-
-	/**
-	 * Method to resolve any signin errors
-	 * */
-	private void resolveSignInError() {
-		if (mConnectionResult.hasResolution()) {
-			try {
-				mIntentInProgress = true;
-				mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
-			} catch (SendIntentException e) {
-				mIntentInProgress = false;
-				mGoogleApiClient.connect();
-			}
-		}
-
-
-		Session session = Session.getActiveSession();
-
-	}
-
-
-	@Override
-	public void onConnectionFailed(ConnectionResult result) {
-		if (!result.hasResolution()) {
-			GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this,
-					0).show();
-			return;
-		}
-
-		if (!mIntentInProgress) {
-			// Store the ConnectionResult for later usage
-			mConnectionResult = result;
-
-			if (mSignInClicked) {
-				// The user has already clicked 'sign-in' so we attempt to
-				// resolve all
-				// errors until the user is signed in, or they cancel.
-				resolveSignInError();
-			}
-		}
-
-	}
-
-
-	private UserInfoChangedCallback createUserInfoChangedCallback() {
-		return new LoginButton.UserInfoChangedCallback() {
-			@Override
-			public void onUserInfoFetched(GraphUser user) {
-
-				if (user != null) {
-
-
-					Intent intent = new Intent(LoginActivity.this,
-							MainActivity.class);
-					startActivity(intent);
-					finish();
-				}
-			}
-		};
-	}
-
-
-
-
-	private void onSessionStateChange(Session session, SessionState state,
-			Exception exception) {
-		// TODO switch by session state
-	}
-
-	public void onActivityResult(int requestCode, int resultCode, int responseCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		// The Facebook login flow requires the users to transition out of, and
-		// back into, this Activity.
-		// That's why we need the onActiviyResult method :)
-		Session.getActiveSession().onActivityResult(this, requestCode,
-				resultCode, data);
-
-		if (requestCode == RC_SIGN_IN) {
-			if (responseCode != RESULT_OK) {
-				mSignInClicked = false;
-			}
-
-			mIntentInProgress = false;
-
-			if (!mGoogleApiClient.isConnecting()) {
-				mGoogleApiClient.connect();
-			}
-		}
-	}
-
-	@Override
-	public void onConnected(Bundle arg0) {
-		mSignInClicked = false;
-		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
-
-		// Get user's information
-		getProfileInformation();
-
-		// Update the UI after signin
-		updateUI(true);
-
-	}
-
-
-
-	/**
-	 * Updating the UI, showing/hiding buttons and profile layout
-	 * */
-	private void updateUI(boolean isSignedIn) {
-		if (isSignedIn) {
-			btnSignIn.setVisibility(View.GONE);
-
-			llProfileLayout.setVisibility(View.VISIBLE);
-		} else {
-			btnSignIn.setVisibility(View.VISIBLE);
-			llProfileLayout.setVisibility(View.GONE);
-		}
-	}
-
-	/**
-	 * Fetching user's information names, email, profile pic
-	 * */
-	private void getProfileInformation() {
-		try {
-			if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
-				Person currentPerson = Plus.PeopleApi
-						.getCurrentPerson(mGoogleApiClient);
-				String personName = currentPerson.getDisplayName();
-				String personPhotoUrl = currentPerson.getImage().getUrl();
-				String personGooglePlusProfile = currentPerson.getUrl();
-				String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
-
-				Log.e(TAG, "Name: " + personName + ", plusProfile: "
-						+ personGooglePlusProfile + ", email: " + email
-						+ ", Image: " + personPhotoUrl);
-
-				txtName.setText(personName);
-				txtEmail.setText(email);
-
-				// by default the profile url gives 50x50 px image only
-				// we can replace the value with whatever dimension we want by
-				// replacing sz=X
-				personPhotoUrl = personPhotoUrl.substring(0,
-						personPhotoUrl.length() - 2)
-						+ PROFILE_PIC_SIZE;
-
-
-
-			} else {
-				Toast.makeText(getApplicationContext(),
-						"Person information is null", Toast.LENGTH_LONG).show();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public void onConnectionSuspended(int arg0) {
-		mGoogleApiClient.connect();
-		updateUI(false);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	/**
-	 * Button on click listener
-	 * */
-	@Override
-	public void onClick(View v) {
-		// creating new user in background thread
-		new CreateNewUser().execute();
-		switch (v.getId()) {
-		case R.id.gplus:
-			// Signin button clicked
-			signInWithGplus();
-			break;
+			// Edit Text
+			inputEmail = (EditText) findViewById(R.id.email);
 			
 
-		}
-	}
+			// Create button
+			Button btnCreateUser = (Button) findViewById(R.id.btnEmail);
 
-	/**
-	 * Sign-in into google
-	 * */
-	private void signInWithGplus() {
-		if (!mGoogleApiClient.isConnecting()) {
-			mSignInClicked = true;
-			resolveSignInError();
-		}
-	}
-	
-	
+			// button click event
+			btnCreateUser.setOnClickListener(new View.OnClickListener() {
 
-	/**
-	 * Background Async Task to Create new user
-	 * */
-	class CreateNewUser extends AsyncTask<String, String, String> {
-
-		/**
-		 * Before starting background thread Show Progress Dialog
-		 * */
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			pDialog = new ProgressDialog(LoginActivity.this);
-			pDialog.setMessage("Logging In..");
-			pDialog.setIndeterminate(false);
-			pDialog.setCancelable(true);
-			pDialog.show();
-		}
-
-		/**
-		 * Creating User
-		 * */
-		protected String doInBackground(String... args) {
-			String email = inputEmail.getText().toString();
-
-
-			// Building Parameters
-			List<NameValuePair> params = new ArrayList<NameValuePair>();
-			params.add(new BasicNameValuePair("email", email));
-
-
-			// getting JSON Object
-			// Note that create user url accepts POST method
-			JSONObject json = jsonParser.makeHttpRequest(url_create_user,
-					"POST", params);
-
-			// check log cat fro response
-			Log.d("Create Response", json.toString());
-
-			// check for success tag
-			try {
-				int success = json.getInt(TAG_SUCCESS);
-
-				if (success == 1) {
-					// successfully created user
-					Intent i = new Intent(getApplicationContext(), MainActivity.class);
-					startActivity(i);
-
-					// closing this screen
-					finish();
-				} else {
-					// failed to create user
+				@Override
+				public void onClick(View view) {
+					// creating new user in background thread
+					new CreateNewUser().execute();
 				}
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-
-			return null;
+			});
 		}
 
 		/**
-		 * After completing background task Dismiss the progress dialog
-		 * **/
-		protected void onPostExecute(String file_url) {
-			// dismiss the dialog once done
-			pDialog.dismiss();
-		}
-
-
-
-		/**
-		 * Background Async task to load user profile picture from url
+		 * Background Async Task to Create new User
 		 * */
-		private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
-			ImageView bmImage;
+		class CreateNewUser extends AsyncTask<String, String, String> {
 
-			public LoadProfileImage(ImageView bmImage) {
-				this.bmImage = bmImage;
+			/**
+			 * Before starting background thread Show Progress Dialog
+			 * */
+			@Override
+			protected void onPreExecute() {
+				super.onPreExecute();
+				pDialog = new ProgressDialog(LoginActivity.this);
+				pDialog.setMessage("Creating User..");
+				pDialog.setIndeterminate(false);
+				pDialog.setCancelable(true);
+				pDialog.show();
 			}
 
-			protected Bitmap doInBackground(String... urls) {
-				String urldisplay = urls[0];
-				Bitmap mIcon11 = null;
+			/**
+			 * Creating User
+			 * */
+			protected String doInBackground(String... args) {
+				String email = inputEmail.getText().toString();
+				
+
+				// Building Parameters
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("email", email));
+				
+
+				// getting JSON Object
+				// Note that create User url accepts POST method
+				JSONObject json = jsonParser.makeHttpRequest(url_create_user,
+						"POST", params);
+				
+				// check log cat fro response
+				Log.d("Create Response", json.toString());
+
+				// check for success tag
 				try {
-					InputStream in = new java.net.URL(urldisplay).openStream();
-					mIcon11 = BitmapFactory.decodeStream(in);
-				} catch (Exception e) {
-					Log.e("Error", e.getMessage());
+					int success = json.getInt(TAG_SUCCESS);
+
+					if (success == 1) {
+						// successfully created user
+						Intent i = new Intent(getApplicationContext(), MainActivity.class);
+						startActivity(i);
+						
+						// closing this screen
+						finish();
+					} else {
+						// failed to create User
+					}
+				} catch (JSONException e) {
 					e.printStackTrace();
 				}
-				return mIcon11;
+
+				return null;
 			}
 
-			protected void onPostExecute(Bitmap result) {
-				bmImage.setImageBitmap(result);
+			/**
+			 * After completing background task Dismiss the progress dialog
+			 * **/
+			protected void onPostExecute(String file_url) {
+				// dismiss the dialog once done
+				pDialog.dismiss();
 			}
+
 		}
 
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		uiHelper.onResume();
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-		uiHelper.onPause();
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		uiHelper.onDestroy();
-	}
-
-
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-		if (Session.getActiveSession() != null || Session.getActiveSession().isOpened()){
-			Intent i = new Intent(LoginActivity.this,MainActivity.class);
-			startActivity(i);
+		@Override
+		public void onConnectionFailed(ConnectionResult arg0) {
+			// TODO Auto-generated method stub
+			
 		}
 
+		@Override
+		public void onConnected(Bundle arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onConnectionSuspended(int arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
-}

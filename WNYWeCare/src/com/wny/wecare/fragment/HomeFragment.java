b2372f20137723dd5d68a32b4d8ab5cb7927577b
infinitycodeservices <infinitycodeservices@gmail.com>
2014-11-09 03:46:19 -0500

    Added search by GPS and updated search by zip

--- WNYWeCare/src/com/wny/wecare/fragment/HomeFragment.java
+++ WNYWeCare/src/com/wny/wecare/fragment/HomeFragment.java
@@ -14,6 +14,12 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,6 +32,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.wny.wecare.MainActivity;
 import com.wny.wecare.R;
 import com.wny.wecare.handler.JSONParser;
@@ -37,6 +45,7 @@ public class HomeFragment extends Fragment implements OnItemSelectedListener, On
 	private Spinner spinner;
 	private Button btnSubmit;
 	private Button zipSubmit;
+	private Button btnLocation;
 	
 	private static final String[] list={"Alden", "Amherst", "Angola",
 		"Blasdell", "Boston", "Bowmansville", "Buffalo", "Cheektowaga", "Clarence",
@@ -62,11 +71,15 @@ public class HomeFragment extends Fragment implements OnItemSelectedListener, On
 
 		btnSubmit = (Button) rootView.findViewById(R.id.town_search);
 		btnSubmit.setOnClickListener(this);
+		
 		spinner =(Spinner)rootView.findViewById(R.id.spinner);
 		spinner.setOnItemSelectedListener(this);
 		
 		zipSubmit = (Button) rootView.findViewById(R.id.zip_search);
 		zipSubmit.setOnClickListener(this);
+		
+		btnLocation = (Button) rootView.findViewById(R.id.btn_location);
+		btnLocation.setOnClickListener(this);
 
 		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item,list);
 		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -116,12 +129,37 @@ public class HomeFragment extends Fragment implements OnItemSelectedListener, On
 			agencySearch(zip);
 			mListener.onFragmentButton();
 			break;
-			/*case R.id.btn_location:
-			String strZip = v.findViewById(R.id.txt_zip).toString();
-			int zip = Integer.parseInt(strZip);
-			agencySearch(zip);
+			case R.id.btn_location:
+				 Geocoder geocoder;
+			     String bestProvider;
+			     List<Address> user = null;
+			     double lat = 0;
+			     double lng = 0;
+			     int radius = 0;
+
+			    LocationManager lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+			     Criteria criteria = new Criteria();
+			     bestProvider = lm.getBestProvider(criteria, false);
+			     Location location = lm.getLastKnownLocation(bestProvider);
+
+			     if (location == null){
+			         Toast.makeText(getActivity(),"Location Not found",Toast.LENGTH_LONG).show();
+			      }else{
+			        geocoder = new Geocoder(getActivity());
+			        try {
+			            user = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+			        lat=(double)user.get(0).getLatitude();
+			        lng=(double)user.get(0).getLongitude();
+			        System.out.println(" DDD lat: " +lat+",  longitude: "+lng);
+
+			        }catch (Exception e) {
+			                e.printStackTrace();
+			        }
+			    }
+			agencySearch(lat, lng, radius);
 			mListener.onFragmentButton();
-			break;*/
+			break;
 		}
 	}
 
@@ -205,6 +243,43 @@ public class HomeFragment extends Fragment implements OnItemSelectedListener, On
 		
 		MainActivity.setResultsList(resultsList);
 
+	}
+	
+	private void agencySearch(double lat, double lng, int radius) {
+		// Setting the URL for the Search by Zip
+				String url_search_agency = "http://www.infinitycodeservices.com/get_agency_by_zip.php";
+				// Building parameters for the search
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("lat", Double.toString(lat)));
+				params.add(new BasicNameValuePair("lng", Double.toString(lng)));
+				params.add(new BasicNameValuePair("radius", Integer.toString(radius)));
+
+				// Getting JSON string from URL
+				JSONArray json = jParser.getJSONFromUrl(url_search_agency, params);
+
+				for (int i = 0; i < json.length(); i++)	{
+					HashMap<String, String> map = new HashMap<String, String>();
+					
+					try	{
+						JSONObject c = (JSONObject) json.get(i);
+						//Fill map
+						Iterator iter = c.keys();
+						while(iter.hasNext())	{
+							String currentKey = (String) iter.next();
+							map.put(currentKey, c.getString(currentKey));
+						}
+						resultsList.add(map);
+						
+					}
+					catch (JSONException e) {
+						e.printStackTrace();
+						
+					}
+					
+				};
+				
+				MainActivity.setResultsList(resultsList);
+		
 	}
 
 
